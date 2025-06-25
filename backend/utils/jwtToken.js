894c4df523c098c9f55256f0be7fc9004fb4655d
Samit Sonkar <samitsonkar0@gmail.com>
2025-06-25 15:02:27 +0530

    changed res.cookie

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -1,13 +1,16 @@
 export const generateToken = (user, message, statusCode, res) => {
   const token = user.generateJsonWebToken();
   const cookieName = user.role === "Admin" ? "adminToken" : "patientToken";
+
+  const cookieExpireDays = Number(process.env.COOKIE_EXPIRE) || 7; // fallback to 7 days
+
   res
     .status(statusCode)
     .cookie(cookieName, token, {
-      expires: new Date(
-        Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-      ),
+      expires: new Date(Date.now() + cookieExpireDays * 24 * 60 * 60 * 1000),
       httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "None",
     })
     .json({
       success: true,
